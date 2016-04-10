d01fbe0945a40d7c8207133d9f88f4e346ec9b1b
leonlxli <Li.Lxiao@yahoo.com>
2016-04-09 17:51:07 -0700

    fixed a small bug

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,18 @@
 // var models = require("../models");
+var mongoose = require('mongoose');
 
 exports.view = function(req, res) {
     /* TODO */
     if (req.user) {
-        res.render("chat");
+        mongoose.model('Posts').find(function(err,posts){
+        console.log(posts);
+		if(err){
+			console.log(err);
+		}
+		else{
+	    	res.render("chat", {'newsfeed': posts});
+		}
+    })
     } else {
         res.redirect("/");
     }
