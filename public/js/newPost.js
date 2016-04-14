c91e677216819f3e2b10310b598a29ec11ff736e
Crystal Nguyen <cmn006@ucsd.edu>
2016-04-14 02:39:34 -0800

    Confirmation messages and error messages when creating new post, and some styling

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -14,3 +14,43 @@
         });
     })
 })($);
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+var errmodal = document.getElementById('errModal');
+// Get the button that opens the modal
+var btn = document.getElementById("submitnewpost");
+// Get the elements that closes the modal
+var span = document.getElementsByClassName("cancelBtn")[0];
+var okBtn = document.getElementById("okBtn");
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    
+    var message = $('#message_content').val();
+    var gym = $('#gym').val();
+
+    if (message != "") {
+        if (gym == "Rimac" || gym == "CanyonView" || gym == "Main_Gym") {
+            $('#postMessage').append("'" + message + "'" + " to " + gym + "?");
+            modal.style.display = "block";
+        }
+        else {
+            //need to choose a gym
+            console.log("error module displayed")
+            errmodal.style.display = "block";
+        }
+    }
+    else {
+        //built in error message with css
+    }
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    $('#postMessage').text("Are you sure you want to post: ");
+    modal.style.display = "none";
+}
+okBtn.onclick = function() {
+    console.log("ok button pressed");
+    errmodal.style.display = "none";
+}
